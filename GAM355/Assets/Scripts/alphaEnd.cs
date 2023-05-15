b7ef9eab50e82d161a0cb037ec116235f2a1963c
PolarstarCoffee <98419830+PolarstarCoffee@users.noreply.github.com>
2023-05-15 15:18:54 -0500

    5/15/23
    Build 1

---
+++ GAM355/Assets/Scripts/alphaEnd.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class alphaEnd : MonoBehaviour
+{
+    //public void OnCollisionEnter(Collision collision)
+    //{
+       // ScenesManager.instance.endofAlpha();
+    //}
+    public void OnTriggerEnter(Collider other)
+    {
+        ScenesManager.instance.endofAlpha();
+    }
+}
