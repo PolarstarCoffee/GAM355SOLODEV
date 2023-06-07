ecab615fe626c7aeee4591b815e4cb557dffe810
Zuriel Zavala <zzavala2@hotmail.com>
2023-06-07 02:05:48 -0500

    6/7/23

--- GAM355/Assets/Scripts/Game/angryNPC.cs
+++ GAM355/Assets/Scripts/Game/angryNPC.cs
@@ -2,29 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class angryNPC : MonoBehaviour
 {
-    public TextMeshProUGUI interact;
+    public Button interact;
     public void OnCollisionStay(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            Debug.Log("enter");
             interact.gameObject.SetActive(true);
-            if (Input.GetKeyDown(KeyCode.T))
-            {
-                ScenesManager.instance.loadAngryNPC();
-            }
-            else
-            {
-                return;
-            }
         }
     }
 
     public void OnCollisionExit(Collision collision)
     {
-        interact.gameObject.SetActive(false);
+        if (collision.gameObject.tag == "Player")
+        {
+            interact.gameObject.SetActive(false);
+        }
     }
+
 }
--- GAM355/Assets/Scripts/Game/barInteract.cs
+++ GAM355/Assets/Scripts/Game/barInteract.cs
@@ -3,29 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class barInteract : MonoBehaviour  
 {
-    public TextMeshProUGUI interact;
+    public Button interact;
     public void OnCollisionStay(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            Debug.Log("enter");
             interact.gameObject.SetActive(true);
-                if (Input.GetKeyDown(KeyCode.T))
-            {
-                ScenesManager.instance.loadBar();
-            }
-            else
-            {
-                return;
-            }
-        }
-    }
 
+        }
+       
+    }
     public void OnCollisionExit(Collision collision)
     {
-        interact.gameObject.SetActive(false);
+        if (collision.gameObject.tag == "Player")
+        {
+            interact.gameObject.SetActive(false);
+        }
     }
 }
